a58ac095e987330e48dd76df650f8aaf737eb51d
Aondona Kato <apostle1aondona@gmail.com>
2025-04-30 22:04:22 +0100

    Initial commit

---
+++ js/menu-highlighter.js
@@ -0,0 +1,31 @@
+// menu-highlighter.js
+
+// This function returns the current file name from the URL
+function currentFile() {
+    // Step 1: Get the current URL
+    var currentURL = window.location.href;
+
+    // Step 2: Find the index of the last slash "/"
+    var fileNameIndex = currentURL.lastIndexOf("/") + 1;
+
+    // Step 3: Extract the file name from the URL
+    var currentFileName = currentURL.substr(fileNameIndex);
+
+    return currentFileName;
+}
+
+// Store the current file name
+var thisFile = currentFile();
+
+// Step 4: If there's no filename (user is at a directory), assume index.html
+if (thisFile === "") {
+    thisFile = "index.html";
+}
+
+// Step 5: Highlight the appropriate menu item
+$("nav a").each(function() {
+    // Check if the href matches the current file
+    if (this.href.indexOf(thisFile) > 0) {
+        $(this).addClass("is-current");
+    }
+});
