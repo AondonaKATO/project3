008390b72ecf2a6c79adaad7b13d83fd4c3c132a
Aondona Kato <apostle1aondona@gmail.com>
2025-05-01 18:59:52 +0100

    Update menu-highlighter.js

--- js/menu-highlighter.js
+++ js/menu-highlighter.js
@@ -22,10 +22,27 @@ if (thisFile === "") {
     thisFile = "index.html";
 }
 
-// Step 5: Highlight the appropriate menu item
+/*/ Step 5: Highlight the appropriate menu item
 $("nav a").each(function() {
     // Check if the href matches the current file
     if (this.href.indexOf(thisFile) > 0) {
         $(this).addClass("is-current");
     }
 });
+
+
+// menu-highlighter.js
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPath = window.location.pathname.split("/").pop(); // Get current filename
+  const navLinks = document.querySelectorAll(".main-menu a");
+
+  navLinks.forEach(link => {
+    const href = link.getAttribute("href");
+    if (href.includes(currentPath)) {
+      link.classList.add("is-current");
+    } else {
+      link.classList.remove("is-current");
+    }
+  });
+});
+*/
